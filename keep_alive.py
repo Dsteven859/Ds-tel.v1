c8061f0d494db2c688b29272b64233089d4dfe29
Dsteven859 <diamanesperalda@gmail.com>
2025-07-02 17:40:43 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from threading import Thread
 import time
@@ -23,10 +22,9 @@ def status():
         "uptime": "24/7"
     }
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
 def keep_alive():
+    def run():
+        app.run(host='0.0.0.0', port=8080, debug=False)
     t = Thread(target=run)
     t.daemon = True
     t.start()
